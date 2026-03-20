080c968ed7e3c074122b1b322b88a4b6ed65d937
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-03-20 19:36:58 +0530

    feat: add cosmos dag for dbt intermediate models

--- dags/cricket_etl.py
+++ dags/cricket_etl.py
@@ -17,6 +17,7 @@ from cosmos import (
     RenderConfig,
 )
 from cosmos.constants import InvocationMode
+from cosmos.config import TestBehavior
 
 from cricketwarehouse import CACHE_DIR
 from cricketwarehouse.cli_util import (
@@ -65,9 +66,20 @@ shared_execution_config = ExecutionConfig(
     },
     tags=["cricwh"]
 )
-def ingest_data():
+def cricket_etl():
     """
-    This is an ingestion pipeline for fetching and ingesting data from Cricsheet.
+    This is an ETL pipeline for fetching, ingesting, and
+    transforming cricsheet ball-by-ball JSON data.
+    Tasks:
+
+    1. `fetch_data_task` -- Fetch and extract JSON ball-by-ball data from Cricsheet.
+    2. `init_source_tables` -- Initialize source tables for raw data (only if needed).
+    3. `ingest_data_task` -- Ingest match data into raw source tables.
+    4. `task_seed` -- Seed `venue_city.csv` and `city_country.csv`
+    5. `task_staging` -- Build staging modes, by staging JSONB data from source tables
+    into `stg_cricsheet_deliveries` and `stg_cricsheet_match_info`
+    6. `task_intermediate` -- Build core models in the intermediate layer,
+    by normalizing data from staging layer.
     """
     @task
     def fetch_data_task():
@@ -131,6 +143,25 @@ def ingest_data():
 
     #-------------------------------------------------
 
+    task_fetch = fetch_data_task()
+    task_init = init_source_tables()
+    task_ingest = ingest_data_task()
+
+    task_seed = DbtTaskGroup(
+        group_id="seed",
+        project_config=ProjectConfig(
+            dbt_project_path=Path(DBT_PROFILES_DIR).as_posix(),
+        ),
+        render_config=RenderConfig(
+            select=["path:seeds"],
+            enable_mock_profile=False,
+            airflow_vars_to_purge_dbt_ls_cache=["purge"]
+        ),
+        execution_config=shared_execution_config,
+        operator_args={"install_deps": True},
+        profile_config=profile_config,
+        default_args={"retries": 0},
+    )
     task_staging = DbtTaskGroup(
         group_id="staging",
         project_config=ProjectConfig(
@@ -147,10 +178,24 @@ def ingest_data():
         default_args={"retries": 0},
     )
 
-    task_fetch = fetch_data_task()
-    task_init = init_source_tables()
-    task_ingest = ingest_data_task()
+    task_intermediate = DbtTaskGroup(
+        group_id="intermediate",
+        project_config=ProjectConfig(
+            dbt_project_path=Path(DBT_PROFILES_DIR).as_posix(),
+        ),
+        render_config=RenderConfig(
+            select=["path:models/intermediate"],
+            enable_mock_profile=False,
+            test_behavior=TestBehavior.NONE,
+            airflow_vars_to_purge_dbt_ls_cache=["purge"]
+        ),
+        execution_config=shared_execution_config,
+        operator_args={"install_deps": True},
+        profile_config=profile_config,
+        default_args={"retries": 0},
+    )
 
-    task_fetch >> task_init >> task_ingest >> task_staging # pyright: ignore[reportUnusedExpression]
+    (task_fetch >>task_init >> task_ingest >>
+     task_seed >> task_staging >> task_intermediate) # pyright: ignore[reportUnusedExpression]
 
-ingest_data()
+cricket_etl()
