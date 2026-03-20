f662b7bac1c2eaf15d4a974f80a9dca5c2c910c0
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-03-20 16:05:15 +0530

    feat: use cosmos to add a dbt task group for staging models

--- dags/ingest.py
+++ dags/ingest.py
@@ -1,12 +1,22 @@
-# ruff: noqa: F401
 from __future__ import annotations
 import pendulum
 from pathlib import Path
 import logging
+import os
+from dotenv import load_dotenv
 
 from airflow.decorators import dag, task
 from airflow.operators.python import get_current_context
 from airflow.models.param import Param
+
+from cosmos import (
+    DbtTaskGroup,
+    ExecutionConfig,
+    ProfileConfig,
+    ProjectConfig,
+    RenderConfig,
+)
+from cosmos.constants import InvocationMode
 
 from cricketwarehouse import CACHE_DIR
 from cricketwarehouse.cli_util import (
@@ -19,6 +29,21 @@ from cricketwarehouse.airflow_util import (
 )
 
 task_logger = logging.getLogger("airflow.task")
+
+load_dotenv()
+
+DBT_PROJECT_NAME = "cricket_warehouse"
+DBT_PROFILES_DIR = os.getenv("DBT_PROFILES_DIR", "/opt/airflow/dbt/")
+
+profile_config = ProfileConfig(
+    profile_name="cricket_warehouse",
+    target_name="dev",
+    profiles_yml_filepath=Path(DBT_PROFILES_DIR) / "profiles.yml"
+)
+
+shared_execution_config = ExecutionConfig(
+    invocation_mode=InvocationMode.SUBPROCESS
+)
 
 @dag(
     schedule=None,
@@ -106,10 +131,26 @@ def ingest_data():
 
     #-------------------------------------------------
 
+    task_staging = DbtTaskGroup(
+        group_id="staging",
+        project_config=ProjectConfig(
+            dbt_project_path=Path(DBT_PROFILES_DIR).as_posix(),
+        ),
+        render_config=RenderConfig(
+            select=["path:models/staging"],
+            enable_mock_profile=False,
+            airflow_vars_to_purge_dbt_ls_cache=["purge"]
+        ),
+        execution_config=shared_execution_config,
+        operator_args={"install_deps": True},
+        profile_config=profile_config,
+        default_args={"retries": 0},
+    )
+
     task_fetch = fetch_data_task()
     task_init = init_source_tables()
     task_ingest = ingest_data_task()
 
-    task_fetch >> task_init >> task_ingest  # pyright: ignore[reportUnusedExpression]
+    task_fetch >> task_init >> task_ingest >> task_staging # pyright: ignore[reportUnusedExpression]
 
 ingest_data()
